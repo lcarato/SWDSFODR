fbe76685dd06e0453fad92c728315ad4b0ea45bd
Luciano Caratori <33559037+lcarato@users.noreply.github.com>
2025-02-28 01:52:32 -0300

    Update shinyLauncher.R

--- R/shinyLauncher.R
+++ R/shinyLauncher.R
@@ -1,7 +1,6 @@
 shinyLauncher_R <- '
 # shinyLauncher.R
 # Provides a function to run the Shiny App located in inst/shiny-app
-
 # Launch the SWDSFODR Shiny Application
 #
 # @description
@@ -10,6 +9,9 @@ shinyLauncher_R <- '
 #
 # @export
 runSWDSFODRApp <- function() {
+# Create directories first
+dir.create("SWDSFODR", showWarnings = FALSE)
+dir.create("SWDSFODR/R", showWarnings = FALSE)
   appDir <- system.file("shiny-app", package="SWDSFODR")
   if (appDir == "") {
     stop("Cannot find Shiny app. Try re-installing `SWDSFODR`.", call. = FALSE)
