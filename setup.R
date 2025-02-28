121e06a8e92b945026c2f7bb5256e2aa7f8741d3
Luciano Caratori <33559037+lcarato@users.noreply.github.com>
2025-02-28 02:45:33 -0300

    Create setup.R

---
+++ setup.R
@@ -0,0 +1,6 @@
+# Directory structure creation
+dir.create("SWDSFODR", showWarnings = FALSE)
+dir.create("SWDSFODR/R", showWarnings = FALSE)
+dir.create("SWDSFODR/man", showWarnings = FALSE)
+dir.create("SWDSFODR/inst", showWarnings = FALSE)
+dir.create("SWDSFODR/inst/shiny-app", showWarnings = FALSE)
