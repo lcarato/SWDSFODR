fa5b082e8ad9572c8c8c77c2e9569c053bb4723a
Luciano Caratori <33559037+lcarato@users.noreply.github.com>
2025-02-28 01:27:35 -0300

    Update calcSWDSEmissions.R

--- R/calcSWDSEmissions.R
+++ R/calcSWDSEmissions.R
@@ -1,24 +1,23 @@
 # calcSWDSEmissions.R
-
-#' Calculate SWDS CH4 Emissions (Annual Model)
-#'
-#' @description
-#'   Implements Equation (1) from Tool 04 (v08.1) to estimate annual CH4 emissions.
-#'
-#' @param W A data frame with columns year, waste_type, mass_tonnes
-#' @param DOCj Named numeric for degradable organic carbon fraction(s)
-#' @param k_j  Named numeric for decay rate(s)
-#' @param phi_y Model correction factor
-#' @param f_y Fraction of CH4 captured
-#' @param GWP_CH4 Global warming potential of CH4
-#' @param OX Oxidation factor
-#' @param F Fraction of CH4 in landfill gas
-#' @param DOCf_y Fraction of degradable organic carbon that decomposes
-#' @param MCF_y Methane correction factor
-#' @param year_target The year for which to calculate CH4 generation
-#'
-#' @return Estimated CH4 emissions in t CO2e
-#'
+# Calculate SWDS CH4 Emissions (Annual Model)
+#
+# @description
+#   Implements Equation (1) from Tool 04 (v08.1) to estimate annual CH4 emissions.
+#
+# @param W A data frame with columns year, waste_type, mass_tonnes
+# @param DOCj Named numeric for degradable organic carbon fraction(s)
+# @param k_j  Named numeric for decay rate(s)
+# @param phi_y Model correction factor
+# @param f_y Fraction of CH4 captured
+# @param GWP_CH4 Global warming potential of CH4
+# @param OX Oxidation factor
+# @param F Fraction of CH4 in landfill gas
+# @param DOCf_y Fraction of degradable organic carbon that decomposes
+# @param MCF_y Methane correction factor
+# @param year_target The year for which to calculate CH4 generation
+#
+# @return Estimated CH4 emissions in t CO2e
+#
 calcSWDSEmissionsYearly <- function(W,
                                     DOCj,
                                     k_j,
