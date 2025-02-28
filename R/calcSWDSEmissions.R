e7fb3729a70288cba94ac83e50fd6edce376d9f1
Luciano Caratori <33559037+lcarato@users.noreply.github.com>
2025-02-28 00:33:44 -0300

    Create calcSWDSEmissions.R

---
+++ R/calcSWDSEmissions.R
@@ -0,0 +1,8 @@
+# calcSWDSEmissions.R
+# Main functions for calculating methane emissions from SWDS
+# according to the equations given in Tool 04 (v08.1).
+
+# ... (entire file content) ...
+# e.g. calcSWDSEmissionsYearly(...) function code,
+#      calcSWDSEmissionsMonthly(...) function code,
+#      with roxygen2 doc comments
