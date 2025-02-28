6f6b126fb8637a1c60bafb2b4b2133591a4a072a
Luciano Caratori <33559037+lcarato@users.noreply.github.com>
2025-02-28 00:38:16 -0300

    Create shinyLauncher.R

---
+++ R/shinyLauncher.R
@@ -0,0 +1,20 @@
+shinyLauncher_R <- '
+# shinyLauncher.R
+# Provides a function to run the Shiny App located in inst/shiny-app
+
+#' Launch the SWDSFOD Shiny Application
+#'
+#' @description
+#'   Opens a Shiny GUI where the user can input data for the FOD model, 
+#'   select approach (annual / monthly / simplified), and see interactive charts/tables.
+#'
+#' @export
+runSWDSFODApp <- function() {
+  appDir <- system.file("shiny-app", package="SWDSFOD")
+  if (appDir == "") {
+    stop("Cannot find Shiny app. Try re-installing `SWDSFOD`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
+'
+writeLines(shinyLauncher_R, "SWDSFOD/R/shinyLauncher.R")
